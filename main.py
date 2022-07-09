db8c7915b8cd8d32e12cc084279a16396abacc61
AmauMaill <amaury.maill@gmail.com>
2022-07-09 13:20:47 +0200

    mvp crawler

--- main.py
+++ main.py
@@ -1,34 +1,93 @@
+from typing import Callable, List
+
 import requests
+import sys
+import getopt
 from bs4 import BeautifulSoup
+import os
 
 BASE_URL = "http://www.gutenberg.org/robot/"
-BASE_QUERY = "harvest?filetypes[]=txt&langs[]=de"
-FIRST_QUERY = BASE_URL + BASE_QUERY
+#BASE_QUERY = "harvest?filetypes[]=txt&langs[]=de"
+#FIRST_QUERY = BASE_URL + BASE_QUERY
+#MAX = 1
 
-MAX = 5 
+def make_query(filetype, language):
+    return f"harvest?filetypes[]={filetype}&langs[]={language}"
 
-response = requests.get(FIRST_QUERY, timeout=5.0)
+def make_url(base: str, query: str) -> str:
+    return base + query
 
-html = response.text
-parsed_html = BeautifulSoup(html)
+def is_http(object: str) -> bool:
+    return object.startswith("http")
 
-print(parsed_html.body.prettify())
+def get(url: str) -> Callable:
+    return requests.get(url, timeout=5.0)
 
-for link in parsed_html.body.find_all('a'):
-    print(link.get('href'))
+def get_stream(url: str) -> Callable:
+    return requests.get(url, stream=True)
 
-URL = BASE_URL + "harvest?offset=112933&filetypes[]=txt&langs[]=de"
+def parse(response: Callable) -> Callable:
+    html = response.text
+    return BeautifulSoup(html, 'html.parser')
 
-response = requests.get(URL, timeout=5.0)
+def list_links(object: Callable) -> List:
+    for link in object.body.find_all('a'):
+        yield link.get("href")
 
-html = response.text
-parsed_html = BeautifulSoup(html)
+def make_directories(path: str) -> None:
+    os.makedirs(path, exist_ok=True)
 
-print(parsed_html.body.prettify())
+def save(stream: Callable, path: str) -> None:
+    with open(path, 'wb') as f:
+        for chunk in stream.iter_content(chunk_size=128):
+            f.write(chunk)
 
-for link in parsed_html.body.find_all('a'):
-    print(link.get('href'))
+def main(argv):
+    filetype = None
+    language = None
+    max = None
 
-links = [link.get('href') for link in parsed_html.body.find_all('a')]
-print([link for link in links if link.startswith("http")])
-print([link for link in links if link.startswith("harvest")])
+    try:
+        opts, args = getopt.getopt(
+            argv, "ht:l:m:", ["filetype=", "language=", "max="]
+        )
+    except getopt.GetoptError:
+        print('main.py -t <filetype> -l <language> -m <max>')
+        sys.exit()
+    
+    for opt, arg in opts:
+        if opt == '-h':
+            print('main.py -t <filetype> -l <language> -m <max>')
+            sys.exit()
+        elif opt in ('-t', '--filetype'):
+            filetype = arg
+        elif opt in ('-l', '--language'):
+            language = arg
+        elif opt in ('-m', '--max'):
+            max = int(arg)
+
+    print(f"Downloading '{filetype}' in '{language}' with '{max}' threshold...")
+
+    first_query = make_query(filetype=filetype, language=language)
+    first_url = make_url(BASE_URL, first_query)
+
+    response = get(first_url)
+    parsed_html = parse(response)
+    links = list_links(parsed_html)
+
+    for i in range(0, max):
+        link = next(links)
+        if is_http(link):
+            stream = get_stream(link)
+            make_directories(f"./data/{language}/")
+            save(stream, f"./data/{language}/{i}.zip")
+        else:
+            url = make_url(BASE_URL, link)
+            response = get(url)
+            parsed_html = parse(response)
+            links = list_links(parsed_html)
+
+    print(f"Saved in './data/{language}/'.")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
